af972b23def959adc13bae9a519b85227607aec1
MeistroBurger <60249244+meistroburger@users.noreply.github.com>
2025-02-14 00:52:54 +0100

    App Icons

---
+++ precache-manifest.535fa1b9de5e2f0aabc42ce497977581.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5846b539b17053380542",
+    "url": "/css/app.fec5b393.css"
+  },
+  {
+    "revision": "110c7773c171d42642ad",
+    "url": "/css/chunk-0173399f.fd69787a.css"
+  },
+  {
+    "revision": "adceb23ee5b3fafc0423",
+    "url": "/css/chunk-13be29f5.e02c82d6.css"
+  },
+  {
+    "revision": "994b4af89a69fa6347d6",
+    "url": "/css/chunk-28a89ab6.01871f80.css"
+  },
+  {
+    "revision": "a75d712a6eded7fe1d64",
+    "url": "/css/chunk-2fe52e9a.2c59716c.css"
+  },
+  {
+    "revision": "48b6b095b6c94dd8382d",
+    "url": "/css/chunk-62ad9a69.dfca84cb.css"
+  },
+  {
+    "revision": "14f3b97744bbdb8e89ff",
+    "url": "/css/chunk-7229211b.16dc44ee.css"
+  },
+  {
+    "revision": "5974b44a1d10fafa9c40",
+    "url": "/css/chunk-781b0d1e.52bd11de.css"
+  },
+  {
+    "revision": "bd51a61ffc42813388ac",
+    "url": "/css/chunk-8a95aeea.09cdc2b4.css"
+  },
+  {
+    "revision": "084e3631e69ad459c36b",
+    "url": "/css/chunk-c066c090.2b6b2e4d.css"
+  },
+  {
+    "revision": "85447ef236c74e87495a53b83e1710d3",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "27cc78678cd03cc43416bdea47bef335",
+    "url": "/icons/android/play_store_512.png"
+  },
+  {
+    "revision": "1adcf52e380c1e81129cd5ea2415b0bb",
+    "url": "/icons/android/res/mipmap-anydpi-v26/ic_launcher.xml"
+  },
+  {
+    "revision": "c3ad9a5b5931560dd97a6510e922a09e",
+    "url": "/icons/android/res/mipmap-hdpi/ic_launcher.png"
+  },
+  {
+    "revision": "59266ff4393bea7aafccb7224d2be8e7",
+    "url": "/icons/android/res/mipmap-hdpi/ic_launcher_background.png"
+  },
+  {
+    "revision": "df50f316bd26f596fea5b6a9bf485896",
+    "url": "/icons/android/res/mipmap-hdpi/ic_launcher_foreground.png"
+  },
+  {
+    "revision": "a2fdf4e8b9060a270b1879e38b8c2376",
+    "url": "/icons/android/res/mipmap-hdpi/ic_launcher_monochrome.png"
+  },
+  {
+    "revision": "6c5c25430e6894c82e30291f96e3579f",
+    "url": "/icons/android/res/mipmap-mdpi/ic_launcher.png"
+  },
+  {
+    "revision": "38b6fce7abbf6174fe0ed649032cbed1",
+    "url": "/icons/android/res/mipmap-mdpi/ic_launcher_background.png"
+  },
+  {
+    "revision": "bc32beab566c382691e7ea7e93e20188",
+    "url": "/icons/android/res/mipmap-mdpi/ic_launcher_foreground.png"
+  },
+  {
+    "revision": "c9d001160d43f98c1585c5868b08e787",
+    "url": "/icons/android/res/mipmap-mdpi/ic_launcher_monochrome.png"
+  },
+  {
+    "revision": "87585586519662a58068f929573352df",
+    "url": "/icons/android/res/mipmap-xhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "a9f100d6c6fe095f3983a2a3f0da2d89",
+    "url": "/icons/android/res/mipmap-xhdpi/ic_launcher_background.png"
+  },
+  {
+    "revision": "a9e8c52874d8342d43be512250b7ea0a",
+    "url": "/icons/android/res/mipmap-xhdpi/ic_launcher_foreground.png"
+  },
+  {
+    "revision": "b5c2f1a17eb8d63f233fbf7a56c5f6ee",
+    "url": "/icons/android/res/mipmap-xhdpi/ic_launcher_monochrome.png"
+  },
+  {
+    "revision": "4bf494e7a365bc8c84b9fb9ebb5905ea",
+    "url": "/icons/android/res/mipmap-xxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "6a752018961c9653f95b796ff5cf106c",
+    "url": "/icons/android/res/mipmap-xxhdpi/ic_launcher_background.png"
+  },
+  {
+    "revision": "d79ff948a5cb9e141ec8f6a481344c52",
+    "url": "/icons/android/res/mipmap-xxhdpi/ic_launcher_foreground.png"
+  },
+  {
+    "revision": "cd4026e740431ffe1e9e99a0b9decd6e",
+    "url": "/icons/android/res/mipmap-xxhdpi/ic_launcher_monochrome.png"
+  },
+  {
+    "revision": "c09856956a7053af49e72faf4f7e9e16",
+    "url": "/icons/android/res/mipmap-xxxhdpi/ic_launcher.png"
+  },
+  {
+    "revision": "961958b352734eb93ab4e0866464336a",
+    "url": "/icons/android/res/mipmap-xxxhdpi/ic_launcher_background.png"
+  },
+  {
+    "revision": "440cf3c6a7fcbd097c1ce2ea75faa2b6",
+    "url": "/icons/android/res/mipmap-xxxhdpi/ic_launcher_foreground.png"
+  },
+  {
+    "revision": "19c4bae68fa0516a4eab6be9d340aaa7",
+    "url": "/icons/android/res/mipmap-xxxhdpi/ic_launcher_monochrome.png"
+  },
+  {
+    "revision": "e1b580660d063b5f40cf1bae1720d7ee",
+    "url": "/icons/androidtv/res/drawable-xhdpi/tv_banner.png"
+  },
+  {
+    "revision": "1620f2110a0d19c6ed65ef95f83425ac",
+    "url": "/icons/ios/AppIcon-20@2x.png"
+  },
+  {
+    "revision": "1620f2110a0d19c6ed65ef95f83425ac",
+    "url": "/icons/ios/AppIcon-20@2x~ipad.png"
+  },
+  {
+    "revision": "dd6fc4fa2ecf8c4f58b58d9e1b0e8f7d",
+    "url": "/icons/ios/AppIcon-20@3x.png"
+  },
+  {
+    "revision": "a41b019f429bf3ff52023322346f552a",
+    "url": "/icons/ios/AppIcon-20~ipad.png"
+  },
+  {
+    "revision": "a3e7393dee8c1bfe8ccd179ae5f14107",
+    "url": "/icons/ios/AppIcon-29.png"
+  },
+  {
+    "revision": "4d9f96d96c278a8e0fa807e57943c8e4",
+    "url": "/icons/ios/AppIcon-29@2x.png"
+  },
+  {
+    "revision": "4d9f96d96c278a8e0fa807e57943c8e4",
+    "url": "/icons/ios/AppIcon-29@2x~ipad.png"
+  },
+  {
+    "revision": "850a8dcc75f99fea276478a3d6918f6c",
+    "url": "/icons/ios/AppIcon-29@3x.png"
+  },
+  {
+    "revision": "a3e7393dee8c1bfe8ccd179ae5f14107",
+    "url": "/icons/ios/AppIcon-29~ipad.png"
+  },
+  {
+    "revision": "09190d5e0a0467f03d084c79f6194368",
+    "url": "/icons/ios/AppIcon-40@2x.png"
+  },
+  {
+    "revision": "09190d5e0a0467f03d084c79f6194368",
+    "url": "/icons/ios/AppIcon-40@2x~ipad.png"
+  },
+  {
+    "revision": "77513d84b97b41cff9c8610b34fc1252",
+    "url": "/icons/ios/AppIcon-40@3x.png"
+  },
+  {
+    "revision": "1620f2110a0d19c6ed65ef95f83425ac",
+    "url": "/icons/ios/AppIcon-40~ipad.png"
+  },
+  {
+    "revision": "77513d84b97b41cff9c8610b34fc1252",
+    "url": "/icons/ios/AppIcon-60@2x~car.png"
+  },
+  {
+    "revision": "67aceac4568be0ce72dc5e898604fd7a",
+    "url": "/icons/ios/AppIcon-60@3x~car.png"
+  },
+  {
+    "revision": "6950fcfad42425662b44918b333dbcb7",
+    "url": "/icons/ios/AppIcon-83.5@2x~ipad.png"
+  },
+  {
+    "revision": "77513d84b97b41cff9c8610b34fc1252",
+    "url": "/icons/ios/AppIcon@2x.png"
+  },
+  {
+    "revision": "09e14d81bd72cdc37407e776bd9da3e3",
+    "url": "/icons/ios/AppIcon@2x~ipad.png"
+  },
+  {
+    "revision": "67aceac4568be0ce72dc5e898604fd7a",
+    "url": "/icons/ios/AppIcon@3x.png"
+  },
+  {
+    "revision": "0ee6a15f8e70d62f6a17628f16fa09eb",
+    "url": "/icons/ios/AppIcon~ios-marketing.png"
+  },
+  {
+    "revision": "c650a3714a59e1dcf28632b0b2e432b7",
+    "url": "/icons/ios/AppIcon~ipad.png"
+  },
+  {
+    "revision": "12ee8441b744cdabc72ddb1cacdd82d1",
+    "url": "/icons/ios/Contents.json"
+  },
+  {
+    "revision": "d7a32f2e505109b514991fdb6c05dfe0",
+    "url": "/icons/macos/AppIcon.icns"
+  },
+  {
+    "revision": "29d248f357b7265d404f479907572f15",
+    "url": "/icons/play-store/play_store_feature_graphic.png"
+  },
+  {
+    "revision": "dffa764380308e9b0ae05a63532ac7b3",
+    "url": "/icons/tvos/1x/background.png"
+  },
+  {
+    "revision": "c42c7f44061405cdfb246df910cc5ab3",
+    "url": "/icons/tvos/1x/foreground.png"
+  },
+  {
+    "revision": "b06f95112b37a65493fe52df4ce0721b",
+    "url": "/icons/tvos/2x/background.png"
+  },
+  {
+    "revision": "a24d3a5aa49642f79c5ed85edf7eebdb",
+    "url": "/icons/tvos/2x/foreground.png"
+  },
+  {
+    "revision": "c344e09bb25c699d3ab20345869060b4",
+    "url": "/icons/tvos/app_store_icon.png"
+  },
+  {
+    "revision": "d56798c6abd2816372e4c0ac49d68009",
+    "url": "/icons/watchos/AppIcon-108@2x~watch.png"
+  },
+  {
+    "revision": "0278229d3e078336f075f5251fcef357",
+    "url": "/icons/watchos/AppIcon-24@2x~watch.png"
+  },
+  {
+    "revision": "f7384dd8bdfb05a0a037cc43ec99c072",
+    "url": "/icons/watchos/AppIcon-27.5@2x~watch.png"
+  },
+  {
+    "revision": "4d9f96d96c278a8e0fa807e57943c8e4",
+    "url": "/icons/watchos/AppIcon-29@2x~watch.png"
+  },
+  {
+    "revision": "850a8dcc75f99fea276478a3d6918f6c",
+    "url": "/icons/watchos/AppIcon-29@3x~watch.png"
+  },
+  {
+    "revision": "09190d5e0a0467f03d084c79f6194368",
+    "url": "/icons/watchos/AppIcon-40@2x~watch.png"
+  },
+  {
+    "revision": "e01d9c2455c3447430615f8afc8a5953",
+    "url": "/icons/watchos/AppIcon-44@2x~watch.png"
+  },
+  {
+    "revision": "90ad3303227ff362557a2619e7332f3d",
+    "url": "/icons/watchos/AppIcon-50@2x~watch.png"
+  },
+  {
+    "revision": "595fe040bac428fd042dee4fffb1f633",
+    "url": "/icons/watchos/AppIcon-86@2x~watch.png"
+  },
+  {
+    "revision": "3020aaee6fc8219a215d7b6e7cbda9eb",
+    "url": "/icons/watchos/AppIcon-98@2x~watch.png"
+  },
+  {
+    "revision": "0ee6a15f8e70d62f6a17628f16fa09eb",
+    "url": "/icons/watchos/AppIcon~watch-marketing.png"
+  },
+  {
+    "revision": "d3df3991a31f034bfa98afdfa3c622e1",
+    "url": "/icons/web/README.txt"
+  },
+  {
+    "revision": "67aceac4568be0ce72dc5e898604fd7a",
+    "url": "/icons/web/apple-touch-icon.png"
+  },
+  {
+    "revision": "241f459aea0929d24646cb76a37244bd",
+    "url": "/icons/web/favicon.ico"
+  },
+  {
+    "revision": "bf620b8af9267b38342e25bfdfd07174",
+    "url": "/icons/web/icon-192-maskable.png"
+  },
+  {
+    "revision": "1ca83e93cf93f978c5c8c15354dbab01",
+    "url": "/icons/web/icon-192.png"
+  },
+  {
+    "revision": "27cc78678cd03cc43416bdea47bef335",
+    "url": "/icons/web/icon-512-maskable.png"
+  },
+  {
+    "revision": "7c319f6a8f1155b0e91af4f4aad17c03",
+    "url": "/icons/web/icon-512.png"
+  },
+  {
+    "revision": "19414d3be35fa2517e236f67f3a3b536",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5846b539b17053380542",
+    "url": "/js/app.d5fb9f8b.js"
+  },
+  {
+    "revision": "110c7773c171d42642ad",
+    "url": "/js/chunk-0173399f.2366f0d6.js"
+  },
+  {
+    "revision": "adceb23ee5b3fafc0423",
+    "url": "/js/chunk-13be29f5.ac94a91e.js"
+  },
+  {
+    "revision": "994b4af89a69fa6347d6",
+    "url": "/js/chunk-28a89ab6.a527599e.js"
+  },
+  {
+    "revision": "eba228cd6300738111d4",
+    "url": "/js/chunk-2d0e2721.39716f46.js"
+  },
+  {
+    "revision": "a75d712a6eded7fe1d64",
+    "url": "/js/chunk-2fe52e9a.5ea4ab9e.js"
+  },
+  {
+    "revision": "48b6b095b6c94dd8382d",
+    "url": "/js/chunk-62ad9a69.08657534.js"
+  },
+  {
+    "revision": "14f3b97744bbdb8e89ff",
+    "url": "/js/chunk-7229211b.89560697.js"
+  },
+  {
+    "revision": "5974b44a1d10fafa9c40",
+    "url": "/js/chunk-781b0d1e.3c9ab5b2.js"
+  },
+  {
+    "revision": "bd51a61ffc42813388ac",
+    "url": "/js/chunk-8a95aeea.7093d32d.js"
+  },
+  {
+    "revision": "084e3631e69ad459c36b",
+    "url": "/js/chunk-c066c090.989c5d97.js"
+  },
+  {
+    "revision": "751b80ea4dc909dd4aa6",
+    "url": "/js/chunk-vendors.ba4dbdf7.js"
+  },
+  {
+    "revision": "3ccfb1de158cdc30851e290a20b1b478",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "5273de9fd981c77ccc475bb34c3066b7",
+    "url": "/service-worker.js"
+  }
+]);
